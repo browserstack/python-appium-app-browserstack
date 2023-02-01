a70b0330e078c1adc1931264dd4593225c4d3aa4
Neha Agarwal <agarneha1331@gmail.com>
2023-02-01 19:18:56 +0530

    Integrating with browserstack-sdk

--- android/browserstack_sample.py
+++ android/browserstack_sample.py
@@ -5,33 +5,8 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
 
-# Options are only available since client version 2.3.0
-# If you use an older client then switch to desired_capabilities
-# instead: https://github.com/appium/python-client/pull/720
-options = UiAutomator2Options().load_capabilities({
-    # Specify device and os_version for testing
-    "platformName" : "android",
-    "platformVersion" : "9.0",
-    "deviceName" : "Google Pixel 3",
-
-    # Set URL of the application under test
-    "app" : "bs://<app-id>",
-
-    # Set other BrowserStack capabilities
-    'bstack:options' : {
-        "projectName" : "First Python project",
-        "buildName" : "browserstack-build-1",
-        "sessionName" : "BStack first_test",
-
-        # Set your access credentials
-        "userName" : "YOUR_USERNAME",
-        "accessKey" : "YOUR_ACCESS_KEY"
-    }
-})
-
 # Initialize the remote Webdriver using BrowserStack remote URL
-# and options defined above
-driver = webdriver.Remote("http://hub.browserstack.com/wd/hub", options=options)
+driver = webdriver.Remote("http://hub.browserstack.com/wd/hub")
 
 # Test case for the BrowserStack sample Android app.
 # If you have uploaded your app, update the test case here.
--- ios/browserstack_sample.py
+++ ios/browserstack_sample.py
@@ -5,31 +5,8 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
 
-# Options are only available since client version 2.3.0
-# If you use an older client then switch to desired_capabilities
-# instead: https://github.com/appium/python-client/pull/720
-options = XCUITestOptions().load_capabilities({
-    # Set URL of the application under test
-    "app" : "bs://<app-id>",
-
-    # Specify device and os_version for testing
-    "deviceName": "iPhone 11 Pro",
-    "platformName": "ios",
-    "platformVersion": "13",
-
-    # Set other BrowserStack capabilities
-    "bstack:options": {
-        "userName" : "YOUR_USERNAME",
-        "accessKey" : "YOUR_ACCESS_KEY",
-        "projectName" : "First Python project",
-        "buildName" : "browserstack-build-1",
-        "sessionName" : "BStack first_test"
-    }
-})
-
 # Initialize the remote Webdriver using BrowserStack remote URL
-# and options defined above
-driver = webdriver.Remote("http://hub.browserstack.com/wd/hub", options=options)
+driver = webdriver.Remote("http://hub.browserstack.com/wd/hub")
 
 # Test case for the BrowserStack sample iOS app.
 # If you have uploaded your app, update the test case here. 
